9729b450022b60fee71f85f6159e6ba81d5a7018
Kartik Thakore <thakore.kartik@gmail.com>
2010-02-07 00:30:45 -0500

    Using blit instead of fill_rect

--- exp/gravity.pl
+++ exp/gravity.pl
@@ -9,6 +9,7 @@ use warnings;
 use SDL;
 use SDL::App;
 use SDL::Video;
+use SDL::Surface;
 use SDL::Rect;
 use SDL::Event;
 use SDL::Events;
@@ -16,50 +17,13 @@ use Data::Dumper;
 
 use Physics::Particles;
 
-my $app = SDL::App->new ( -title => 'Gravity Sim', -width => 800, -height => 600 );
+SDL::init(SDL_INIT_VIDEO);
+
+my $app = SDL::Video::set_video_mode (  800,  600, 32, SDL_HWSURFACE | SDL_DOUBLEBUF );
+my $app_rect = SDL::Rect->new(0,0,800,  600);
 my @old_part_rect;
 my @new_part_rect;
 my @color;
-
-my $black = SDL::Video::map_RGB ( $app->format(), 0, 0, 0 );
-sub update
-{
-
- SDL::Video::fill_rect
-       ( $app, SDL::Rect->new(0, 0, $app->w, $app->h), $black );
-
-}
-
-sub rand_color
-{
-   my $r =rand( 30 +255 ) - 30;
-   my $b =rand( 30 +255 ) - 30;
-   my $g =rand( 30 +255 ) - 30;
-
-  return SDL::Video::map_RGB ( $app->format(), $r, $g, $b );
-
-
-}
-
-sub update_particle
-{
-
- my $p = shift;
-
-  $color[$p->{n}] = rand_color if !defined $color[$p->{n}];
-
-  my $c = $color[$p->{n}];
-
- my $size =  $p->{m};
- my $new_part_rect =SDL::Rect->new($p->{x}-$size/2, $p->{y} - $size/2, $size, $size);
-
- SDL::Video::fill_rect
-        ( $app, $new_part_rect, $c);
-
-  SDL::Video::update_rects
-        ( $app,  $old_part_rect[$p->{n}], $new_part_rect )  if(  defined  $old_part_rect[$p->{n}] );
-  $old_part_rect[$p->{n}] = $new_part_rect;
-}
 
 
 use constant G      => 0.03;
@@ -125,6 +89,37 @@ $sim->add_particle(
 
 my $event = SDL::Event->new();
 
+my $cont = 1;
+while($cont){
+
+	while (SDL::Events::poll_event($event)) {
+		    $cont = 0 if $event->type == SDL_QUIT;
+		      }
+		      
+		      update();
+	
+	$sim->iterate_step(1);
+}
+
+
+sub update_bg
+{
+	
+return	SDL::Video::fill_rect( $app, $app_rect, SDL::Video::map_RGB ( $app->format, 0, 0, 10 ));
+	
+
+}
+
+sub init_particle_surf
+{
+	my $size = shift;
+	my $bg = SDL::Surface->new(SDL_SWSURFACE, $size, $size, 32, 0, 0, 0, 0);
+SDL::Video::fill_rect( $bg, SDL::Rect->new(0, 0, $size, $size), rand_color());
+
+	SDL::Video::display_format( $bg );
+		return $bg;
+}
+
 sub warp
 {
    my $p = shift;
@@ -137,22 +132,52 @@ sub warp
 
 }
 
-my $cont = 1;
-while($cont){
-
-	while (SDL::Events::poll_event($event)) {
-		    $cont = 0 if $event->type == SDL_QUIT;
-		      }
-	foreach my $p (@{ $sim->{p} }) {
+sub update
+{
+	
+ update_bg();
+ foreach my $p (@{ $sim->{p} }) {
 
 #		print Dumper $p;
-		update();
+		
 		update_particle($p);
 		warp($p)
 
 	}
-	$sim->iterate_step(1);
+
+ SDL::Video::flip($app);
+
+
+}
+
+sub rand_color
+{
+   my $r =rand( 30 +255 ) - 30;
+   my $b =rand( 30 +255 ) - 30;
+   my $g =rand( 30 +255 ) - 30;
+
+  return SDL::Video::map_RGB ( $app->format, $r, $g, $b );
+}
+
+sub update_particle
+{
+
+ my $p = shift;
+ my $size =  $p->{m};
+  $color[$p->{n}] = init_particle_surf($size) if !defined $color[$p->{n}];
+
+  my $c = $color[$p->{n}];
+
+ my $new_part_rect = SDL::Rect->new(0, 0, $size, $size);
+
+SDL::Video::blit_surface( $c, $new_part_rect, $app, SDL::Rect->new($p->{x}-$size/2, $p->{y} - $size/2, $app->w, $app->h) );
+  
+  #SDL::Video::update_rects ( $app, $old_part_rect[$p->{n}], $new_part_rect ) if( defined $old_part_rect[$p->{n}] );
+  $old_part_rect[$p->{n}] = $new_part_rect;
+
+ 
 }
 
 
-SDL::quit
+
+
